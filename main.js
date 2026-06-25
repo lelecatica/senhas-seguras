1cb0f060eba5a76b3a72016cd75169a6db9c06ec
lelecatica <malanhuk.leticia@escola.pr.gov.br>
2026-06-25 08:49:31 -0300

    f

---
+++ main.js
@@ -0,0 +1,13 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+
+numeroSenha.textContent = 5;
+
+const botoes = document.querySelectorAll('.parametro-senha-botoes'); 
+
+botoes[0].onclick = diminuiTamanho;
+
+function diminuiTamanho(){
+    
+}
+console.log(botoes);
+
